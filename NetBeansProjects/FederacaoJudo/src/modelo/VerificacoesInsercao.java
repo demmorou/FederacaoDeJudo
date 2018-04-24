82f5d29c6f4797210db05305e69338efd803d149
oziel <ozielcarvalho@live.com>
2018-04-24 16:23:36 -0300

    Validações

--- NetBeansProjects/FederacaoJudo/src/modelo/VerificacoesInsercao.java
+++ NetBeansProjects/FederacaoJudo/src/modelo/VerificacoesInsercao.java
@@ -5,16 +5,53 @@
  */
 package modelo;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author deusimar
  */
 public class VerificacoesInsercao {
     
-    public boolean AltercaoAluno(String academia, String cpf, String comp, String graduacao, String mae, String nome, String ourtoga, String fone, int idade){
+    public boolean AltercaoAluno(String academia, String cpf, String comp, String graduacao, String mae, String nome, String ourtoga, String fone, String idade, String peso){
+                if(academia.equals("")){
+            JOptionPane.showInputDialog("Informe um Nome");
+            return false;
+        }
+        if(cpf.equals("") && new Validar().isCPF(cpf) == false){
+            JOptionPane.showInputDialog("Informe um CPF Válido");
+            return false;
+        }
+        if(comp.equals("")){
+            JOptionPane.showInputDialog("Informe as Competições");
+            return false;
+        }
+        if(graduacao.equals("")){
+            JOptionPane.showInputDialog("Informe a Graduação");
+            return false;
+        }
+        if(mae.equals("")){
+            JOptionPane.showInputDialog("Informe o nome da Mãe");
+            return false;
+        }
+        if(ourtoga.equals("")){
+            JOptionPane.showInputDialog("Informe a data de Ourtoga");
+            return false;
+        }
+        if(fone.equals("")){
+            JOptionPane.showInputDialog("Informe o Telefone");
+            return false;
+        }
+        if(Integer.parseInt(idade) < 0 && Integer.parseInt(idade) > 150){
+            JOptionPane.showInputDialog("Informe uma Idade Válida");
+            return false;
+        }
         
+        if(Float.parseFloat(peso) < 0.0 &&  Float.parseFloat(peso) > 500){
+            JOptionPane.showInputDialog("infome um peso Válido");
+            return false;
+        }
         
-    
         return true;
     }
 }
