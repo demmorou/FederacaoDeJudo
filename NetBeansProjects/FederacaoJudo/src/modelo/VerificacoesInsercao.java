436d733d3cb1385ca86f1a97b7fb7ddd3bd7cd9c
oziel <ozielcarvalho@live.com>
2018-04-24 16:53:58 -0300

    Validações(Correção)

--- NetBeansProjects/FederacaoJudo/src/modelo/VerificacoesInsercao.java
+++ NetBeansProjects/FederacaoJudo/src/modelo/VerificacoesInsercao.java
@@ -14,41 +14,49 @@ import javax.swing.JOptionPane;
 public class VerificacoesInsercao {
     
     public boolean AltercaoAluno(String academia, String cpf, String comp, String graduacao, String mae, String nome, String ourtoga, String fone, String idade, String peso){
+        
         if(academia.equals("")){
-            JOptionPane.showInputDialog("Informe um Nome");
+            JOptionPane.showMessageDialog(null,"Informe um Nome");
             return false;
         }
+        
         if(cpf.equals("") && new Validar().isCPF(cpf) == false){
-            JOptionPane.showInputDialog("Informe um CPF Válido");
+            JOptionPane.showMessageDialog(null,"Informe um CPF Válido");
             return false;
         }
+        
         if(comp.equals("")){
-            JOptionPane.showInputDialog("Informe as Competições");
+            JOptionPane.showMessageDialog(null,"Informe as Competições");
             return false;
         }
+        
         if(graduacao.equals("")){
-            JOptionPane.showInputDialog("Informe a Graduação");
+            JOptionPane.showMessageDialog(null,"Informe a Graduação");
             return false;
         }
+        
         if(mae.equals("")){
-            JOptionPane.showInputDialog("Informe o nome da Mãe");
+            JOptionPane.showMessageDialog(null,"Informe o nome da Mãe");
             return false;
         }
+        
         if(ourtoga.equals("")){
-            JOptionPane.showInputDialog("Informe a data de Ourtoga");
+            JOptionPane.showMessageDialog(null,"Informe a data de Ourtoga");
             return false;
         }
+        
         if(fone.equals("")){
-            JOptionPane.showInputDialog("Informe o Telefone");
+            JOptionPane.showMessageDialog(null,"Informe o Telefone");
             return false;
         }
+        
         if(Integer.parseInt(idade) < 0 && Integer.parseInt(idade) > 150){
-            JOptionPane.showInputDialog("Informe uma Idade Válida");
+            JOptionPane.showMessageDialog(null,"Informe uma Idade Válida");
             return false;
         }
         
         if(Float.parseFloat(peso) < 0.0 &&  Float.parseFloat(peso) > 500){
-            JOptionPane.showInputDialog("infome um peso Válido");
+            JOptionPane.showMessageDialog(null,"Informe um peso Válido");
             return false;
         }
         
