41b9a06200a5581261d274b7330b75f1c8d01dbd
Deusimar <sousadeusimar2@gmail.com>
2018-04-25 19:27:51 -0300

    Verificacoes de inserção

--- NetBeansProjects/FederacaoJudo/src/modelo/VerificacoesInsercao.java
+++ NetBeansProjects/FederacaoJudo/src/modelo/VerificacoesInsercao.java
@@ -11,54 +11,59 @@ import javax.swing.JOptionPane;
  *
  * @author deusimar
  */
-
 public class VerificacoesInsercao {
-    
-    public boolean AltercaoAluno(String cpf, String comp, String graduacao, String mae, String nome, String ourtoga, String fone, String idade, String peso){
-        
-        if(cpf.equals("") && new Validar().isCPF(cpf) == false){
-            JOptionPane.showMessageDialog(null,"Informe um CPF Válido");
+
+    public boolean AltercaoAluno(String cpf, String comp, String graduacao, String mae, String nome, String ourtoga, String fone, String idade, String peso) {
+
+        if (cpf.equals("") && new Validar().isCPF(cpf) == false) {
+            JOptionPane.showMessageDialog(null, "Informe um CPF Válido");
             return false;
         }
-        
-        if(comp.equals("")){
-            JOptionPane.showMessageDialog(null,"Informe as Competições");
+
+        if (comp.equals("")) {
+            JOptionPane.showMessageDialog(null, "Informe as Competições");
             return false;
         }
-        
-        if(graduacao.equals("")){
-            JOptionPane.showMessageDialog(null,"Informe a Graduação");
+
+        if (graduacao.equals("")) {
+            JOptionPane.showMessageDialog(null, "Informe a Graduação");
             return false;
         }
-        
-        if(mae.equals("")){
-            JOptionPane.showMessageDialog(null,"Informe o nome da Mãe");
+
+        if (mae.equals("")) {
+            JOptionPane.showMessageDialog(null, "Informe o nome da Mãe");
             return false;
         }
-        
-        if(ourtoga.equals("")){
-            JOptionPane.showMessageDialog(null,"Informe a data de Ourtoga");
+
+        if (ourtoga.equals("")) {
+            JOptionPane.showMessageDialog(null, "Informe a data de Ourtoga");
             return false;
         }
-        
-        if(fone.equals("")){
-            JOptionPane.showMessageDialog(null,"Informe o Telefone");
+
+        if (fone.equals("")) {
+            JOptionPane.showMessageDialog(null, "Informe o Telefone");
             return false;
         }
-        
-        if(Integer.parseInt(idade) < 0 && Integer.parseInt(idade) > 150){
-            JOptionPane.showMessageDialog(null,"Informe uma Idade Válida");
+
+        if (idade.contains("^[a-Z]")) {
+            JOptionPane.showMessageDialog(null, "Informe uma Idade Válida");
+            return false;
+        } else if (Integer.parseInt(idade) < 0 && Integer.parseInt(idade) > 150) {
+            JOptionPane.showMessageDialog(null, "Informe uma Idade Válida");
             return false;
         }
-        
-        if(peso.contains("^[a-Z]") == false){
-            JOptionPane.showMessageDialog(null,"Informe um peso Válido");
+
+        if (peso.contains("^[a-Z]")) {
+            JOptionPane.showMessageDialog(null, "Informe um peso Válido");
             return false;
-        }else if((Float.parseFloat(peso) < 0.0 &&  Float.parseFloat(peso) > 500)){
-                JOptionPane.showMessageDialog(null,"Informe um peso Válido");
+        } else {
+            float p = Float.valueOf(peso.replace(",", "."));
+            if (p < 0.0 && p > 500.0) {
+                JOptionPane.showMessageDialog(null, "Informe um peso Válido");
                 return false;
             }
-        
+        }
+
         return true;
     }
 }
