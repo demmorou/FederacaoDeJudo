cfc0c220e20cd0658bb4ffa40daecf00aa1cfe14
Oziel <ozielcarvalho@live.com>
2018-05-15 16:22:24 -0300

    Bibliotecas

--- NetBeansProjects/FederacaoJudo/test/br/tests/FederacaoJudo/TestsCadastro.java
+++ NetBeansProjects/FederacaoJudo/test/br/tests/FederacaoJudo/TestsCadastro.java
@@ -1,5 +1,26 @@
 package br.tests.FederacaoJudo;
 
+import modelo.Aluno;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestsCadastro {
-    CadastroAluno 
+    Aluno a;
+   
+        @Before
+	public void setup() {
+		a = new Aluno();
+	}
+
+	@After
+	public void teardown() {
+		a = null;
+	}
+        
+        @Test
+	public void testJogadaValidaVezJogador(){
+            
+            
+        }
 }
