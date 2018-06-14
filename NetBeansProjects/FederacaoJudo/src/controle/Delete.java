57515d2d620a9d53ef831bc775aa32d017fb1f4f
Deusimar <sousadeusimar2@gmail.com>
2018-06-14 16:11:18 -0300

    delete academia

--- NetBeansProjects/FederacaoJudo/src/controle/Delete.java
+++ NetBeansProjects/FederacaoJudo/src/controle/Delete.java
@@ -1,5 +1,11 @@
 package controle;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author deusimar
@@ -8,7 +14,15 @@ public class Delete {
     
     public boolean deleteAcademia(int id){
     
-    
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null, st = null;
+        
+        try {
+            stmt = con.prepareStatement("DELETE FROM academia WHERE Id_academia = "+id);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        
         return true;
     }
 }
