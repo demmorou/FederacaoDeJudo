aa89e4521d07a36b07f1a8eb83d7cc84914856cf
Oziel <ozielcarvalho@live.com>
2018-06-18 14:18:17 -0300

    cadastro academia

--- NetBeansProjects/FederacaoJudo/src/modelo/CadastrarAcademia.java
+++ NetBeansProjects/FederacaoJudo/src/modelo/CadastrarAcademia.java
@@ -10,25 +10,25 @@ public class CadastrarAcademia {
     
     public boolean DadosCadastroAcademia(Academia a){
         
-        if(a.getNomeAcademia().equals("") || !new VerificarString().verificaString(a.getNomeAcademia()) || a.getNomeAcademia().length() < 2 || a.getNomeAcademia().length() > 40 )
+        if(a.getNomeAcademia().equals("") || !new VerificarString().verificaString(a.getNomeAcademia()) || a.getNomeAcademia().length() < 2 || a.getNomeAcademia().length() > 40 || !a.getNomeAcademia().matches("[a-zA-Z\\s]+"))
             return false;
         
-        if(a.getBairro().equals("") || !new VerificarString().verificaString(a.getBairro()) || a.getBairro().length() < 2 || a.getBairro().length() > 40)
+        if(a.getBairro().equals("") || !new VerificarString().verificaString(a.getBairro()) || a.getBairro().length() < 2 || a.getBairro().length() > 40 || !a.getBairro().matches("[a-zA-Z\\s]+"))
             return false;
         
         if(a.getCep().equals("") || !Validar.isCep(a.getCep()))
             return false;
         
-        if(a.getEstado().equals("") || !new VerificarString().verificaString(a.getEstado()) || a.getEstado().length() < 2 || a.getEstado().length() > 40)
+        if(a.getEstado().equals("") || !new VerificarString().verificaString(a.getEstado()) || a.getEstado().length() < 2 || a.getEstado().length() > 40 || !a.getEstado().matches("[a-zA-Z\\s]+"))
             return false;
         
         if(a.getNumero() <= 0)
             return false;
-        
-        if(a.getRua().equals("") || !new VerificarString().verificaString(a.getRua()) || a.getRua().length() < 2 || a.getRua().length() > 40)
+        System.out.println(a.getRua());
+        if(a.getRua().equals("") || !new VerificarString().verificaString(a.getRua()) || a.getRua().length() < 2 || a.getRua().length() > 40 || !a.getRua().matches("[a-zA-Z\\s]+"))
             return false;
         
-        if(a.getCidade().equals("") || !new VerificarString().verificaString(a.getCidade()) || a.getCidade().length() < 2 || a.getCidade().length() > 40)
+        if(a.getCidade().equals("") || !new VerificarString().verificaString(a.getCidade()) || a.getCidade().length() < 2 || a.getCidade().length() > 40 || !a.getCidade().matches("[a-zA-Z\\s]+"))
             return false;
         
         return new DAO().createAcademia(a);
--- NetBeansProjects/FederacaoJudo/src/modelo/UpdateAcademia.java
+++ NetBeansProjects/FederacaoJudo/src/modelo/UpdateAcademia.java
@@ -10,34 +10,25 @@ public class UpdateAcademia {
     
     public boolean dadosUpdateAcademia(Academia a){
         
-        if(a.getNomeAcademia().equals("") 
-                || !new VerificarString().verificaString(a.getNomeAcademia())
-                || a.getNomeAcademia().length() < 2)
+        if(a.getNomeAcademia().equals("") || !new VerificarString().verificaString(a.getNomeAcademia()) || a.getNomeAcademia().length() < 2 || a.getNomeAcademia().length() > 40 || !a.getNomeAcademia().matches("[a-zA-Z\\s]+"))
             return false;
         
-        if(a.getBairro().equals("") 
-                || !new VerificarString().verificaString(a.getBairro())
-                || a.getBairro().length() < 2)
+        if(a.getBairro().equals("") || !new VerificarString().verificaString(a.getBairro()) || a.getBairro().length() < 2 || a.getBairro().length() > 40 || !a.getBairro().matches("[a-zA-Z\\s]+"))
             return false;
         
-        if(a.getCep().equals("") || !Validar.isCep(a.getCep())
-                || a.getCep().length() != 9)
+        if(a.getCep().equals("") || !Validar.isCep(a.getCep()))
             return false;
         
-        if(a.getEstado().equals("") 
-                || !new VerificarString().verificaString(a.getEstado())
-                || a.getEstado().length() < 2)
+        if(a.getEstado().equals("") || !new VerificarString().verificaString(a.getEstado()) || a.getEstado().length() < 2 || a.getEstado().length() > 40 || !a.getEstado().matches("[a-zA-Z\\s]+"))
             return false;
         
         if(a.getNumero() <= 0)
             return false;
         
-        if(a.getRua().equals("") || a.getRua().length() < 2)
+        if(a.getRua().equals("") || !new VerificarString().verificaString(a.getRua()) || a.getRua().length() < 2 || a.getRua().length() > 40 || !a.getRua().matches("[a-zA-Z\\s]+"))
             return false;
         
-        if(a.getCidade().equals("") 
-                || !new VerificarString().verificaString(a.getCidade())
-                || a.getCidade().length() < 2)
+        if(a.getCidade().equals("") || !new VerificarString().verificaString(a.getCidade()) || a.getCidade().length() < 2 || a.getCidade().length() > 40 || !a.getCidade().matches("[a-zA-Z\\s]+"))
             return false;
         
         return new Update().updateAcademia(a);
