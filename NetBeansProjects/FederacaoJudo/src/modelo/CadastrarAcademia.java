6d411a6248211867e3efd8c810af42e3445b84fb
Oziel <ozielcarvalho@live.com>
2018-06-18 12:29:10 -0300

    Testes

--- NetBeansProjects/FederacaoJudo/src/modelo/CadastrarAcademia.java
+++ NetBeansProjects/FederacaoJudo/src/modelo/CadastrarAcademia.java
@@ -10,25 +10,25 @@ public class CadastrarAcademia {
     
     public boolean DadosCadastroAcademia(Academia a){
     
-        if(a.getNomeAcademia().equals("") || !new VerificarString().verificaString(a.getNomeAcademia()))
+        if(a.getNomeAcademia().equals("") || !new VerificarString().verificaString(a.getNomeAcademia()) || 2 < a.getNomeAcademia().length() || a.getNomeAcademia().length() > 40 )
             return false;
         
-        if(a.getBairro().equals("") || !new VerificarString().verificaString(a.getBairro()))
+        if(a.getBairro().equals("") || !new VerificarString().verificaString(a.getBairro()) || 2 < a.getBairro().length() || a.getBairro().length() > 40)
             return false;
         
         if(a.getCep().equals("") || !Validar.isCep(a.getCep()))
             return false;
         
-        if(a.getEstado().equals("") || !new VerificarString().verificaString(a.getEstado()))
+        if(a.getEstado().equals("") || !new VerificarString().verificaString(a.getEstado()) || 2 < a.getEstado().length() || a.getEstado().length() > 40)
             return false;
         
         if(a.getNumero() <= 0)
             return false;
         
-        if(a.getRua().equals("") || !new VerificarString().verificaString(a.getRua()))
+        if(a.getRua().equals("") || !new VerificarString().verificaString(a.getRua()) || 2 < a.getRua().length() || a.getRua().length() > 40)
             return false;
         
-        if(a.getCidade().equals("") || !new VerificarString().verificaString(a.getCidade()))
+        if(a.getCidade().equals("") || !new VerificarString().verificaString(a.getCidade()) || 2 < a.getCidade().length() || a.getCidade().length() > 40)
             return false;
         
         return new DAO().createAcademia(a);
